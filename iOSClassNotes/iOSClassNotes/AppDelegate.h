3bb7f559407a803a2397b6c5998b5d5dd4b7fc64
Slilianyou <li.lian.you163@163.com>
2016-09-06 13:52:06 +0800

    创建

---
+++ iOSClassNotes/iOSClassNotes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSClassNotes
+//
+//  Created by ss-iOS-LLY on 16/9/6.
+//  Copyright © 2016年 lilianyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSClassNotes/iOSClassNotes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSClassNotes
+//
+//  Created by ss-iOS-LLY on 16/9/6.
+//  Copyright © 2016年 lilianyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
