84e947bfb8e7ae591110ec702c3c84f45dbc651d
Slilianyou <li.lian.you163@163.com>
2016-09-06 10:24:34 +0800

    ww

---
+++ iOSClassNotes/iOSClassNotes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iOSClassNotes
+//
+//  Created by ss-iOS-LLY on 16/9/6.
+//  Copyright © 2016年 lilianyou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
